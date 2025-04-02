0991fc6803f9d02c6c74db4ba4673aeb3b529299
Dawid Zapadka <dawid-zapadka@o2.pl>
2025-04-02 11:20:11 +0200

    add prod version

--- src/components/ui/LoadingSpinner/LoadinSpinner.tsx
+++ src/components/ui/LoadingSpinner/LoadinSpinner.tsx
@@ -1,5 +1,17 @@
 import styles from "./LoadingSpinner.module.scss";
 
 export function LoadingSpinner() {
-  return <div>Loading Spinner...</div>;
+  return (
+    <div className={styles.loadingSpinnerContainer}>
+      <div className={styles.loadingSpinner}>
+        <div
+          className={`${styles.spinnerSector} ${styles.spinnerSectorBlue}`}
+        ></div>
+        <div
+          className={`${styles.spinnerSector} ${styles.spinnerSectorWhite}`}
+        ></div>
+      </div>
+      <p className={styles.loadingText}>Ładowanie...</p>
+    </div>
+  );
 }
